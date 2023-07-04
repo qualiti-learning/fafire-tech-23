2b95323c3b9171d2698fbab4f542f4e1bf0f6ab4
Keven Leone <keven.santos.sz@gmail.com>
2023-07-03 21:58:21 -0300

    Intro

---
+++ intro.js
@@ -0,0 +1,16 @@
+let number = 1;
+let value = `keven ${number}`;
+
+value = 1;
+value = 1.23;
+value = 10_000_000;
+value = true;
+value = new Date();
+value = [];
+value = undefined;
+value = null;
+
+number += 1; // number = number + 1
+number -= 1; // number = number - 1
+number /= 1;
+number *= 1;
