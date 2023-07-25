9950cd99e5ad372b8151c970a15ae8882d920410
Keven Leone <keven.santos.sz@gmail.com>
2023-07-24 21:58:22 -0300

    React with Shortner

--- packages/api/src/index.js
+++ packages/api/src/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
 
 const shortnerRouter = require('./routes/ShortnerRouter');
 
 const app = express();
+
+app.use(helmet());
+app.use(cors());
 
 app.use(morgan('dev'));
 app.use(express.json());
--- packages/web/src/App.jsx
+++ packages/web/src/App.jsx
@@ -1,70 +1,101 @@
-import './index.css';
+import {
+  Alert,
+  Button,
+  Container,
+  Heading,
+  Input,
+  Link,
+  Stack,
+  Text,
+  useToast,
+} from '@chakra-ui/react';
+import { useState } from 'react';
 
-const Layout = (props) => (
-  <div>
-    <Navbar />
-
-    {props.children}
-
-    <Footer />
-  </div>
-);
-
-const Navbar = () => (
-  <nav className='navbar navbar-expand-lg '>
-    <div className='container-fluid'>
-      <a className='navbar-brand' href='#'>
-        Navbar
-      </a>
-      <button
-        className='navbar-toggler'
-        type='button'
-        data-bs-toggle='collapse'
-        data-bs-target='#navbarNav'
-        aria-controls='navbarNav'
-        aria-expanded='false'
-        aria-label='Toggle navigation'
-      >
-        <span className='navbar-toggler-icon'></span>
-      </button>
-      <div className='collapse navbar-collapse' id='navbarNav'>
-        <ul className='navbar-nav'>
-          <li className='nav-item'>
-            <a className='nav-link active' aria-current='page' href='#'>
-              Home
-            </a>
-          </li>
-          <li className='nav-item'>
-            <a className='nav-link' href='#'>
-              Features
-            </a>
-          </li>
-          <li className='nav-item'>
-            <a className='nav-link' href='#'>
-              Pricing
-            </a>
-          </li>
-          <li className='nav-item'>
-            <a className='nav-link disabled'>Disabled</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
-);
-
-const Footer = () => (
-  <footer>
-    <div>Copyright - Fafire Tech {new Date().getFullYear()}</div>
-  </footer>
-);
+const API_DOMAIN = 'http://byte.li';
 
 const App = () => {
+  const [shortner, setShortner] = useState();
+  const [value, setValue] = useState('');
+  const toast = useToast();
+
+  const onClickShortnerURL = async () => {
+    if (!value) {
+      return alert('The link is missing');
+    }
+
+    const response = await fetch(`${API_DOMAIN}/api/shortner`, {
+      body: JSON.stringify({
+        link: value,
+        author: 'Keven',
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'POST',
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+      setShortner(data);
+
+      return toast({
+        title: 'Shortner created.',
+        description: "We've created your shortned link for you.",
+        status: 'success',
+        duration: 5000,
+        isClosable: true,
+        position: 'bottom-left',
+      });
+    }
+
+    toast({
+      title: 'Failed',
+      description: data?.message,
+      status: 'error',
+      duration: 5000,
+      isClosable: true,
+      position: 'bottom-left',
+    });
+  };
+
   return (
-    <Layout>
-      <h1>Fafire Tech - Turma 1</h1>
-      <button className='btn btn-warning'>Click here</button>
-    </Layout>
+    <Container p={4}>
+      <Heading>Free URL Shortener</Heading>
+      <Text color='gray'>
+        Byte.li is a free tool to shorten URLs powered by Rebrandly. Create
+        short & memorable links in seconds.
+      </Text>
+
+      <Stack display='flex'>
+        <Input
+          placeholder='Enter link here'
+          type='text'
+          value={value}
+          onChange={(event) => {
+            setValue(event.target.value);
+          }}
+        />
+        <Button onClick={onClickShortnerURL}>Shortner URL</Button>
+      </Stack>
+
+      {shortner && (
+        <Alert display='flex' flexDirection='column'>
+          <Link
+            href={`${API_DOMAIN}/${shortner.hash}`}
+            isExternal
+          >{`${API_DOMAIN}/${shortner.hash}`}</Link>
+          <Link href={shortner.link} isExternal>
+            {shortner.link}
+          </Link>
+        </Alert>
+      )}
+
+      <Text color='gray'>
+        By clicking Shorten URL, you agree to Rebrandly's Terms of Use, Privacy
+        Policy and Cookie Policy
+      </Text>
+    </Container>
   );
 };
 
